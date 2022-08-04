c7fb5d4da436d7dc35e81a161527611825a4d837
seungmin Ryu <ojysep9987@naver.com>
2022-08-05 01:02:21 +0900

    배포 test

--- backend/settings.py
+++ backend/settings.py
@@ -15,8 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-with open(os.path.join(BASE_DIR, 'secrets.json'), 'rb') as secret_file:
-    secrets = json.load(secret_file)
+# with open(os.path.join(BASE_DIR, 'secrets.json'), 'rb') as secret_file:
+#     secrets = json.load(secret_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
