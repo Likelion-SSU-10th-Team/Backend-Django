ce30b58496254b918bd4f8cfe6af2c63508c8b2e
seungmin Ryu <ojysep9987@naver.com>
2022-08-06 03:05:23 +0900

    TEST: DB 테스트코드

--- album/views.py
+++ album/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import *
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
+@csrf_exempt
+def album_list(request):
+
+    albums = Album.objects.all()
+    list = []
+    for album in albums:
+        list.append(
+            {
+                "album_name": album.name
+            }
+        )
+    return JsonResponse({"albums": list}, status=200)
