f697e01e4b7558cc1c3ab69a3ce773fcabaf406d
seungmin Ryu <ojysep9987@naver.com>
2022-08-09 19:36:15 +0900

    Feature: COOKIES(session 기반 login, logout) 기반 개발

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,75 @@
 from django.shortcuts import render
+import json
+import bcrypt
+from .models import User
 
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, JsonResponse
+
+
+def register(request):
+    print(request.body)
+    data = json.loads(request.body)
+
+    try:
+        if User.objects.filter(email=data['email']).exists():
+            return JsonResponse({"message": "EXISTS_EMAIL"}, status=400)
+        User.objects.create(
+            email=data['email'],
+            password=bcrypt.hashpw(data["password"].encode("UTF-8"), bcrypt.gensalt()).decode("UTF-8"),
+            name=data['name'],
+            age=data['age']
+        ).save()
+        return HttpResponse(status=200)
+
+    except KeyError:
+        return JsonResponse({"message": "INVALID_KEYS"}, status=400)
+
+
+def login(request):
+    data = json.loads(request.body)
+    user = User.objects.get(email=data["email"])
+
+    try: # email이랑 password로 존재하는 회원가입을 하였는지 체크
+        if User.objects.filter(email=data["email"]).exists()\
+                and bcrypt.checkpw(data['password'].encode('UTF-8'), user.password.encode('UTF-8')):
+            session_id = user.email+user.password
+            user.session_id = session_id
+            user.save()
+            # chrome이든 edge든 response에 session_id 값 만들어서 리턴해주기
+            response = HttpResponse("로그인 성공", status=200)
+            response.set_cookie('session_id', session_id)
+            return response
+
+        return HttpResponse(status=400)
+
+    except KeyError:
+        return JsonResponse({'message': "INVALID_KEYS"}, status=400)
+
+
+def logout(request):
+    # session_id랑 user가 일치하는지 확인하는 부분도 필요할 듯?
+    try:
+        user = User.objects.get(session_id=request.COOKIES.get('session_id'))
+        if user.session_id:
+            user.session_id = ""
+            user.save()
+
+            response = HttpResponse("로그아웃 성공", status=200)
+            response.delete_cookie('session_id')
+            return response
+
+        return HttpResponse(status=400)
+
+    except KeyError:
+        return JsonResponse({'message': "INVALID_KEYS"}, status=400)
+
+
+def session(request):
+    print(request.COOKIES)
+    print(request.COOKIES.get('sessionid')) # edge
+    print(request.COOKIES.get('JSESSIONID')) # chrome
+    response = HttpResponse('session 테스트')
+    response.set_cookie('test', 'test')
+    return response
