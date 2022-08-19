5c93b1bf08b9193a14f29dd7b6e1c6d338e5c569
seungmin Ryu <ojysep9987@naver.com>
2022-08-19 16:31:45 +0900

    Fix: url 수정

--- album/urls.py
+++ album/urls.py
@@ -5,7 +5,7 @@ import diary.views as d_views
 urlpatterns = [
     path('', views.album_list), # 앨범 페이지 정보 / GET ✔
     path('new/', views.make_album), # 앨범 만들기 / POST ✔
-    path('<int:album_id>', views.album_detail), # album 하나의 디테일 data / GET ✔
+    path('<int:album_id>/', views.album_detail), # album 하나의 디테일 data / GET ✔
     path('edit/<int:diary_id>', views.read_page), # Read 페이지 데이터 다 쏴줌 / GET ✔
     # path('<int:album_id>/<int:diary_id>/', views.diary_detail), # diary 정보 data / GET
     # path('composition/<int:diary_id>', views.send_album_id), # 일기가 속한 앨범 / GET
